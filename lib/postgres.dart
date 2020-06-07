191a713612512fcf7a84a7eb75137e278bf91b12
bettdouglas <bettdalpha@gmail.com>
2020-06-07 17:56:30 +0300

    added the various geometric types in Dart

--- lib/postgres.dart
+++ lib/postgres.dart
@@ -1,5 +1,7 @@
 library postgres;
 
+export 'package:dart_jts/dart_jts.dart';
+export 'package:postgres/src/geometry_parser.dart';
 export 'src/connection.dart';
 export 'src/execution_context.dart';
 export 'src/substituter.dart';
